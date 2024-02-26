f3f3798652d8d1a6124ae2978eb9e3518e63e5e7
seonghyeoklee <dltjdgur327@gmail.com>
2024-02-26 11:15:02 +0900

    feature - 읽기, 쓰기 서비스 분리

--- app/service-internal-api/src/main/java/com/dongrami/todo/application/TodoReadService.java
+++ app/service-internal-api/src/main/java/com/dongrami/todo/application/TodoReadService.java
@@ -1,8 +1,6 @@
 package com.dongrami.todo.application;
 
 import com.dongrami.todo.domain.TodoEntity;
-import com.dongrami.todo.dto.request.RequestCreateTodoDto;
-import com.dongrami.todo.dto.request.RequestUpdateTodoDto;
 import com.dongrami.todo.dto.response.ResponseTodoDto;
 import com.dongrami.todo.repository.TodoRepository;
 import com.dongrami.todo.repository.support.TodoSearchDto;
@@ -13,31 +11,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
-public class TodoService {
+public class TodoReadService {
     private final TodoRepository todoRepository;
 
-    public void createTodo(RequestCreateTodoDto requestCreateTodoDto) {
-        todoRepository.save(requestCreateTodoDto.toEntity());
-    }
-
-    public void updateTodo(Long id, RequestUpdateTodoDto requestUpdateTodoDto) {
-        TodoEntity todoEntity = todoRepository.findById(id)
-                .orElseThrow();
-
-        todoEntity.update(
-                requestUpdateTodoDto.getTitle(),
-                requestUpdateTodoDto.getContent(),
-                requestUpdateTodoDto.getTodoStatus()
-        );
-    }
-
-    public void deleteTodo(Long id) {
-        todoRepository.deleteById(id);
-    }
-
-    @Transactional(readOnly = true)
     public ResponseTodoDto getTodoById(Long id) {
         TodoEntity todoEntity = todoRepository.findById(id)
                 .orElseThrow();
@@ -45,7 +23,6 @@ public class TodoService {
         return ResponseTodoDto.from(todoEntity);
     }
 
-    @Transactional(readOnly = true)
     public Page<ResponseTodoDto> getTodoPageBySearch(Pageable pageable, TodoSearchDto todoSearchDto) {
         Page<TodoEntity> todoRepositoryBySearch = todoRepository.findBySearch(pageable, todoSearchDto);
 
---
+++ app/service-internal-api/src/main/java/com/dongrami/todo/application/TodoWriteService.java
@@ -0,0 +1,77 @@
+package com.dongrami.todo.application;
+
+import com.dongrami.todo.domain.TodoEntity;
+import com.dongrami.todo.domain.TodoNotificationEntity;
+import com.dongrami.todo.domain.TodoStatus;
+import com.dongrami.todo.dto.request.RequestCreateTodoDto;
+import com.dongrami.todo.dto.request.RequestUpdateTodoDto;
+import com.dongrami.todo.dto.response.ResponseTodoDto;
+import com.dongrami.todo.repository.TodoRepository;
+import com.dongrami.user.application.UserService;
+import com.dongrami.user.domain.UserEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class TodoWriteService {
+    private final UserService userService;
+    private final TodoRepository todoRepository;
+
+    public ResponseTodoDto createTodo(String username, RequestCreateTodoDto request) {
+
+        // 1. 사용자 조회
+        UserEntity userEntity = userService.getUser(username);
+
+        // 2-1 알림이 존재하지 않는 경우
+        if(request.getNotificationTime() == null) {
+            TodoEntity todoEntity = TodoEntity.create(
+                    request.getContent(),
+                    request.getMemo(),
+                    TodoStatus.TODO,
+                    userEntity
+            );
+
+            return ResponseTodoDto.from(todoRepository.save(todoEntity));
+        }
+
+        // 2-2 알림이 존재하는 경우
+        TodoNotificationEntity todoNotificationEntity = TodoNotificationEntity.builder()
+                .startDate(LocalDate.now())
+                .endDate(LocalDate.now())
+                .notificationTime(request.getNotificationTime())
+                .excludedDates(null)
+                .userEntity(userEntity)
+                .build();
+
+        TodoEntity todoEntity = TodoEntity.create(
+                request.getContent(),
+                request.getMemo(),
+                TodoStatus.TODO,
+                userEntity
+        );
+
+        todoEntity.addTodoNotification(todoNotificationEntity);
+
+        return ResponseTodoDto.from(todoRepository.save(todoEntity));
+    }
+
+    public void updateTodo(Long id, RequestUpdateTodoDto requestUpdateTodoDto) {
+        TodoEntity todoEntity = todoRepository.findById(id)
+                .orElseThrow();
+
+        todoEntity.update(
+                requestUpdateTodoDto.getContent(),
+                requestUpdateTodoDto.getTodoStatus()
+        );
+    }
+
+    public void deleteTodo(Long id) {
+        todoRepository.deleteById(id);
+    }
+
+}
